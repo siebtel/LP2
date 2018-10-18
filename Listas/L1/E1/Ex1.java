cbf7244af7d7b32be73083329055ff09a35d7754
Thiago Lima <thiago@scrapinghub.com>
2018-10-18 12:17:16 -0300

    Ex1 com toda a logica, mas sem Exception customizadas

--- Listas/L1/E1/Ex1.java
+++ Listas/L1/E1/Ex1.java
@@ -1,64 +1,112 @@
 public class Ex1 {
-    
-    public static void main (String args[ ] ) {
+
+    public static void main(String args[]) {
         double area = 0.0;
 
-        switch(args.length) {
+        try {
+            double[] valores = Ex1.converterArgumentosEmNumero(args);
+
+            switch (valores.length) {
             case 1:
-                double raio = Double.parseDouble(args[0]);
-                area = Ex1.calcula(raio);
+                area = Ex1.calcula(valores[0]);
                 Ex1.printArea("circulo", area);
                 break;
-            
+
             case 2:
-                double base = Double.parseDouble(args[0]);
-                double altura = Double.parseDouble(args[1]);
-                area = Ex1.calcula(base, altura);
+                area = Ex1.calcula(valores[0], valores[1]);
                 Ex1.printArea("retangulo", area);
                 break;
-            
-            case 3:
-                double l1 = Double.parseDouble(args[0]);
-                double l2 = Double.parseDouble(args[1]);
-                double l3 = Double.parseDouble(args[2]);
-                area = Ex1.calcula(l1, l2, l3);
+                
+                case 3:
+                area = Ex1.calcula(valores[0], valores[1], valores[2]);
+                String tipoTriangulo = Ex1.getTipoTriangulo(valores[0], valores[1], valores[2]);
                 Ex1.printArea("triangulo", area);
+                System.out.println("O triangulo e " + tipoTriangulo + ".");
                 break;
+            }
 
-            default:
-                int n = args.length;
-                Ex1.printError(
-                    "Numero incorreto de argumentos (" + String.valueOf(n) + ")", 
-                    "O programa deve receber de 1 a 3 argumentos."
-                );
-
+        } catch (Exception e) {
+            String message = e.getMessage();
+            System.out.println(message);
         }
 
-        
-   }
+    }
 
-   private static double calcula(double r) {
-       return r * r * 3.14;
-   }
-    
+    private static double calcula(double r) {
+        return r * r * Math.PI;
+    }
+
     private static double calcula(double b, double a) {
         return b * a;
     }
-    
-    private static double calcula(double l1, double l2, double l3) {
+
+    private static double calcula(double l1, double l2, double l3) throws Exception {
+        if (!Ex1.isTriangulo(l1, l2, l3)) {
+            throw new Exception("Os lados informados não geram um triângulo.");
+        }
+
         // FORMULA DE HERON
         double p = (l1 + l2 + l3) / 2;
         double area = Math.sqrt(p * (p - l1) * (p - l2) * (p - l3));
+
         return area;
+    }
+
+    private static double[] converterArgumentosEmNumero(String args[]) throws Exception {
+        if (args.length < 1) {
+            throw new Exception("Numero de argumentos insuficiente.");
+        }
+        if (args.length > 3) {
+            throw new Exception("Numero de argumentos excessivo.");
+        }
+
+        double[] valores = new double[args.length];
+        String erro = "";
+
+        for (int i = 0; i < args.length; i++) {
+            try {
+                valores[i] = Double.parseDouble(args[i]);
+            } catch (Exception e) {
+                if (erro.trim().length() > 0) {
+                    erro += '\n';
+                }
+                erro += String.valueOf(i+1) + "o argumento, \"" + String.valueOf(args[i]) + "\", nao eh numero.";
+            }
+        }
+
+        if (erro.trim().length() > 0) {
+            throw new Exception(erro);
+        }
+
+        return valores;
+    }
+
+    private static boolean isTriangulo(double l1, double l2, double l3) {
+        boolean a = Ex1.calculaCondicaoTriangulo(l1, l2, l3);
+        boolean b = Ex1.calculaCondicaoTriangulo(l1, l2, l3);
+        boolean c = Ex1.calculaCondicaoTriangulo(l1, l2, l3);
+
+        return a && b && c;
+    }
+
+    private static boolean calculaCondicaoTriangulo(double lado, double c1, double c2) {
+        return lado > Math.abs(c1 - c2) && lado < (c1 + c2);
+    }
+
+    private static String getTipoTriangulo(double l1, double l2, double l3) {
+        if (l1 == l2 && l2 == l3) {
+            return "equilatero";
+        } 
+
+        if (l1 != l2 && l2 != l3) {
+            return "escaleno";
+        }
+
+        return "isosceles";
     }
 
     private static void printArea(String poligono, double area) {
         System.out.println("A area do " + poligono + " e: " + String.format("%.2f", area) + " unidades de area");
-    }
-
-    private static void printError(String tipo, String message) {
-        System.out.println("ERRO: " + tipo);
-        System.out.println("\u2192 " + message);
     }
 
 }
